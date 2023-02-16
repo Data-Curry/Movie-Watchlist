ffa00fadea634babac45879dd7472aba2383f6e1
Christopher Austin <66553323+Data-Curry@users.noreply.github.com>
2023-02-16 16:21:17 +0900

    Change SELECT_WATCHED_MOVIES to a JOIN query; Change prompt_print_watched_movies to make use of it.

--- app.py
+++ app.py
@@ -38,17 +38,19 @@ def print_movie_list(heading, movies):
     print("----\n")
 
 
-def print_watched_movie_list(username, movies):
-    print(f"-- {username}'s watched movies --")
-    for movie in movies:
-        print(f"{movie[1]}")
-    print("---- \n")
-
-
 def prompt_watch_movie():
     username = input("Username: ")
     movie_id = input("Movie ID: ")
     database.watch_movie(username, movie_id)
+
+
+def prompt_print_watched_movies():
+    username = input("Username: ")
+    movies = database.get_watched_movies(username)
+    if movies:
+        print_movie_list(f"{username}'s Watched", movies)
+    else:
+        print(f"{username} hasn't watched any movies yet.")
 
 
 def prompt_add_user():
@@ -68,9 +70,7 @@ while (user_input := input(menu)) != "7":
     elif user_input == "4":
         prompt_watch_movie()
     elif user_input == "5":
-        username = input("Username: ")
-        movies = database.get_watched_movies(username)
-        print_watched_movie_list(username, movies)
+        prompt_print_watched_movies()
     elif user_input == "6":
         prompt_add_user()
     else:
